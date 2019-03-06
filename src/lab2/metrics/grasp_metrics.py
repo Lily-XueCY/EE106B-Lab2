40f84060d8041af32bf8d9fcf158c7a3cd21b885
ryanogorman <41647011+ryanogorman@users.noreply.github.com>
2019-03-06 09:16:47 -0800

    Force closure & gravity resistance (still need ferrari canny)

--- src/lab2/metrics/grasp_metrics.py
+++ src/lab2/metrics/grasp_metrics.py
@@ -6,7 +6,7 @@ Author: Chris Correa
 # may need more imports
 import numpy as np
 from lab2.utils import vec, adj, look_at_general
-import cvxpy as cvx
+import cvxpy as cp
 import math
 
 def compute_force_closure(vertices, normals, num_facets, mu, gamma, object_mass):
@@ -36,7 +36,40 @@ def compute_force_closure(vertices, normals, num_facets, mu, gamma, object_mass)
     float : quality of the grasp
     """
     # YOUR CODE HERE
-    raise NotImplementedError
+    # check if vert2 in vert1 cone
+    score = 0
+    p1 = vertices[0:3]
+    p2 = vertices[3:]
+    norm1 = normals[0:3]
+    norm2 = normals[3:]
+    
+    if is_in_cone(p1,p2,norm1,mu) and is_in_cone(p2,p1,norm2,mu):
+        score = 1
+    return score
+
+def is_in_cone(p1,p2,norm1,mu):
+    """
+    Author: Ryan
+    Determines if p2 is in the friction cone of p1
+
+    Parameters
+    ----------
+    p1 : coordinate of point 1 in world frame
+    p2 : coordinate of point 2 in world frame
+    norm1 : normal direction of force 1 in world frame
+    mu : coefficient of friction
+
+    Returns
+    -------
+    bool : whether p2 is in the friction cone of p1
+    """
+    Gwa = look_at_general(p1,norm1)
+    pa = np.dot(Gwa,p2)
+    if math.sqrt(pa[0]**2+pa[1]**2) <= mu*pa[2]:
+        return True
+    else:
+        return False
+
 
 def get_grasp_map(vertices, normals, num_facets, mu, gamma):
     """ 
@@ -61,8 +94,17 @@ def get_grasp_map(vertices, normals, num_facets, mu, gamma):
     -------
     :obj:`numpy.ndarray` grasp map
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    # YOUR CODE HERE 
+    # Ryan code, uses utils for most of the work
+    finger = np.matrix([[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,0], [0,0,0,0], [0,0,0,1]])
+    rot1 = look_at_general(vertices[0],normals[0])
+    adj1 = adj(rot1)
+    rot2 = look_at_general(vertices[1],normals[1])
+    adj2 = adj(rot2)
+    G1 = np.dot(adj1,finger)
+    G2 = np.dot(adj2,finger)
+    G = np.hstack((G1,G2))
+    return G
 
 def contact_forces_exist(vertices, normals, num_facets, mu, gamma, desired_wrench):
     """
@@ -90,7 +132,21 @@ def contact_forces_exist(vertices, normals, num_facets, mu, gamma, desired_wrenc
     bool : whether contact forces can produce the desired_wrench on the object
     """
     # YOUR CODE HERE
-    raise NotImplementedError
+    thresh = 1
+    G = get_grasp_map(vertices, normals, num_facets, mu, gamma)
+    f = cp.variable(8)
+    objective = cp.Minimize(cp.norm2(G*f-desired_wrench)) # sum_squares probably works fine
+    # Im unsure if absolute is allowed
+    contraint = [np.absolute(f[3])-gamma*f[2]<=0, np.absolute(f[7])-gamma*f[6]<=0, np.absolute(f[0])+np.absolute(f[1])-mu*f[2]<=0, np.absolute(f[4])+np.absolute(f[5])-mu*f[6]<=0, -f[2]<=0, -f[6]<=0]
+    prob = cp.Problem(objective, contraint)
+    # try catch would probably be useful
+    prob.solve()
+    if problem.status not in ["infeasible", "unbounded"]: # do I have to have some sort of threshold
+        result = prob.value
+        F = np.dot(G,result)
+        if np.linalg.norm(F,desired_wrench) <= thresh:
+            return True
+    return False
 
 def compute_gravity_resistance(vertices, normals, num_facets, mu, gamma, object_mass):
     """
@@ -118,7 +174,11 @@ def compute_gravity_resistance(vertices, normals, num_facets, mu, gamma, object_
     float : quality of the grasp
     """
     # YOUR CODE HERE (contact forces exist may be useful here)
-    raise NotImplementedError
+    score = 0
+    W = np.transpose(np.array([0 0 -9.81*object_mass 0 0 0]))
+    if contact_forces_exist(vertices, normals, num_facets, mu, gamma, W):
+        score = 1
+    return score
 
 def compute_custom_metric(vertices, normals, num_facets, mu, gamma, object_mass):
     """
